d340d3dd5c48e5408d39579e5a5425fb7335fd57
Raul <rsa.raulsanz@gmail.com>
2023-05-08 00:58:24 +0200

    base de datos

--- backend/database.js
+++ backend/database.js
@@ -1,8 +1,9 @@
 /**************     Configuracion de la base de datos     ****************/
 const mongoose = require('mongoose');
 const URI = 'mongodb://127.0.0.1:27017/TriFast';
+const URI_ATLAS = 'mongodb+srv://rsaraulsanz:Rw2mchM6Y8HiEqDy@trifast.rhzapec.mongodb.net/?retryWrites=true&w=majority';
 
-mongoose.connect(URI)
+mongoose.connect(URI_ATLAS)
     .then(db => console.log('Base de datos conectada'))
     .catch(err => console.error(err));
 
