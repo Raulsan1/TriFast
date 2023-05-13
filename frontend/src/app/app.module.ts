2c00a3588beebae79bdb68b5940687ad96eb3fde
Raul <rsa.raulsanz@gmail.com>
2023-05-14 01:41:22 +0200

    gran avance en la parte de front

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,19 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProductsComponent } from './components/products/products.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FeaturedProductsComponent } from './components/featured-products/featured-products.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { SeguimientosComponent } from './components/seguimientos/seguimientos.component';
+import { authGuard } from './guards/auth.guard';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent,
+    NavbarComponent,
+    FeaturedProductsComponent,
+    LoginComponent,
+    SignupComponent,
+    PrincipalComponent,
+    SeguimientosComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
